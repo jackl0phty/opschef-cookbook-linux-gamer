f573ecd20cb92b88d4da3a291537c827754d3174
Gerald L. Hevener Jr <hevenerg@marshall.edu>
2013-01-23 23:14:28 -0500

    Edit metadata.rb & regenerate metadata.json

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,7 @@
 name		 "linux-gamer"
 maintainer       "Gerald L. Hevener Jr., M.S."
 maintainer_email "jackl0phty@gmail.com"
-license          "Apache 2.0"
+license          ""
 description      "Installs/Configures open source games on GNU/Linux."
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.0.4"
@@ -9,3 +9,4 @@ depends		 "apt"
 
 %w{ debian ubuntu }.each do |os|
   supports os
+end
