25f50f902002211cd6bbb5dd933276b71d38a8e2
Gerald L. Hevener Jr <hevenerg@marshall.edu>
2013-01-23 22:58:19 -0500

    Add cookbook name to metadata.rb.

--- metadata.rb
+++ metadata.rb
@@ -1,6 +1,7 @@
+name		 "linux-gamer"
 maintainer       "Gerald L. Hevener Jr., M.S."
 maintainer_email "jackl0phty@gmail.com"
-license          ""
+license          "Apache 2.0"
 description      "Installs/Configures open source games on GNU/Linux."
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.0.4"
